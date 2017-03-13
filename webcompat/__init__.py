a7d7d6f5f3d81548570177adb2e56a12e7914caf
Beatriz Rizental Machado <beatriz.rizental@gmail.com>
2017-03-13 12:16:58 +0100

    Revert "Fixes #1420 - Updates the usage of Flask-Limiter"

--- webcompat/__init__.py
+++ webcompat/__init__.py
@@ -10,7 +10,6 @@ import logging
 
 from flask_github import GitHub
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from flask import Flask
 
 app = Flask(__name__, static_url_path='')
@@ -22,7 +21,7 @@ app.config.from_object('config')
 app.config['MAX_CONTENT_LENGTH'] = 5.5 * 1024 * 1024
 
 github = GitHub(app)
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(app)
 
 # import views after we initialize our github object
 import webcompat.views
